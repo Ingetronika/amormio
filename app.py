30c42a57af9b551e50abd20cf2290ed2fe5c72ec
Ingetronika <95087048+Ingetronika@users.noreply.github.com>
2024-09-26 22:27:39 -0500

    primer comit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        numero = request.form.get('numero')
+        return render_template('resultado.html', numero=numero)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
